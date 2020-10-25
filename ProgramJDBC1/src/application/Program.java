cd0804a46c5d6b42619baaa60859f20fc787b673
Alex Amorim <obr.alexalexamorim@gmail.com>
2020-10-25 04:48:39 -0300

    usando JDBC - connection

---
+++ ProgramJDBC1/src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		Connection conn = DB.getConnection();
+		DB.closeConnection();
+
+	}
+
+}
---
+++ ProgramJDBC1/src/db/DB.java
@@ -0,0 +1,51 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection() throws ClassNotFoundException {
+		if(conn == null){
+			try {
+				Properties properties = loadProperties();
+				String url = properties.getProperty("dburl");
+				String user = properties.getProperty("user");
+				String password = properties.getProperty("password");
+				
+				conn = DriverManager.getConnection(url, user, password);
+				
+			} catch (SQLException e) {
+				throw new DBException(e.getMessage());
+			} 
+		}
+		return conn;
+	}
+	
+	private static Properties loadProperties() {
+		try(FileInputStream fis = new FileInputStream("db.properties")) {
+			Properties properties = new Properties();
+			properties.load(fis);			
+			return properties;
+		} catch (IOException e) {
+			throw new DBException(e.getMessage());
+		}
+		
+	}
+	
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DBException(e.getMessage());
+			}
+		}
+	}
+}
